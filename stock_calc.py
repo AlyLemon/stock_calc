28f8a40b352b304cd66a5374392a5fe6a38f5263
Aly <lilyivytulip@gmail.com>
2024-03-24 00:08:12 -0500

    updated

--- stock_calc.py
+++ stock_calc.py
@@ -13,20 +13,20 @@ local_css("styles.css")
 def price_calculation():
     global market_cap, supply
 
-    result=int(market_cap)/int(supply)
+    result=(market_cap)/(supply)
     result=round(result,2)
     st.write(f"${result} USD")
 
-market_cap=st.text_input("What is the Market Cap?")
-supply=st.text_input("What is the Circulating Supply?")
+market_cap=st.number_input("What is the Market Cap?")
+supply=st.number_input("What is the Circulating Supply?")
 price_calculation()
 
 # this is the main code for market cap calc
 def market_calculation():
     global price, supply
-    result=int(supply)*int(price)
+    result=(supply)*(price)
     result=round(result,2)
     st.write(f"${result} USD")
 
-price=st.text_input("What is the Price?")
+price=st.number_input("What is the Price?")
 market_calculation()
